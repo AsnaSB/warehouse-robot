e3b863bc2d7461b5c77c298863340579d161ce06
Govind <govindkdl2005@gmail.com>
2026-09-21 16:52:42 +0530

    Add warehouse simulation and visualization

--- src/environment/warehouse_env.py
+++ src/environment/warehouse_env.py
@@ -120,6 +120,7 @@ class WarehouseEnv(_BaseEnv):
         # --------------------------------------------------------------
 
         self.robot_pos = None
+        self.start_pos = None
         self.goal_pos = None
         self._step_count = 0
 
@@ -199,9 +200,13 @@ class WarehouseEnv(_BaseEnv):
 
         self._step_count = 0
 
+        
+
         self.robot_pos = (
             self._random_free_cell()
         )
+
+        self.start_pos = self.robot_pos
 
         self.goal_pos = (
             self._random_free_cell(
